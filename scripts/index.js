77718939cd856f521205f126565402fd100a7dc2
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-09-22 11:35:03 +1200

    Added original repo code

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ function inputLength() {
     return input.value.length; // returns length of value found in "input" variable.
 }
 
-function createListElement() {
+/* function createListElement() {
     var li = document.createElement("li"); // declares variable with "li" tag.
     li.appendChild(document.createTextNode(input.value)); // creates a text node for "li" variable with the value of "input" variable.
     ul.appendChild(li); // appends "ul" tag with "li" variable
@@ -35,7 +35,39 @@ function createListElement() {
     });
     // revert input value back to nothing
     input.value = ""
+} */
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+
+	li.addEventListener("click", function () {
+		// creates a boolean that toggles the done class on li:
+		// if the list item is clicked this toggles the done class
+		var finished = this.classList.toggle("done");
+		// creates a remove button for the finished item:
+		var removeButton = document.createElement("button");
+		removeButton.classList.add("deleteButton");
+
+		// if the list item is clicked (li add event listener ) then
+		// finished is true
+		if (finished) {
+			removeButton.appendChild(document.createTextNode("remove"));
+			removeButton.classList = "deleteButton";
+			li.appendChild(removeButton);
+
+			removeButton.addEventListener("click", function () {
+				this.parentElement.remove();
+			});
+		} else {
+			this.getElementsByClassName("deleteButton")[0].remove();
+		}
+	});
+	// revert input value back to nothing
+	input.value = "";
 }
+
 
 function addListAfterClick() {
     if (inputLength() > 0) {
